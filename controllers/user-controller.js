39d83eaf1a8b73ac4ea04aaff633654705010191
Varun Master <varunmaster95@gmail.com>
2019-11-29 12:41:32 -0500

    added controller methods

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -12,6 +12,35 @@ const getAllPostings = (req, res) => {
         });
 };
 
+const getPostingsSavedByUser = (req, res) => {
+    knex.select("*").from("subscription").where(`userID = ${req.params.id}`) //ensure that the id is passed into the url such as /api/userSavedPosting/{userid} or something similar
+        //this id should be saved in the localstorage or session storage after login of user:
+        // sessionStorage.setItem({
+        //     userid: "82"
+        // }); 
+        // sessionStorage.getItem("userid");
+        .then(data => {
+            res.json(data); //can make another api call in here to get the name of the posting instead of just the id 
+        })
+        .catch(err => {
+            console.log(err);
+            res.json(err);
+        });
+};
+
+const getUsersFromPosting = (req, res) => {
+    knex.select("*").from("subscription").where(`postingID = ${req.params.id}`) //ensure that the id is passed into the url such as /api/userSavedPosting/{userid} or something similar
+    .then(data => {
+        res.json(data); //can make another api call in here to get the user info 
+    })
+    .catch(err => {
+        console.log(err);
+        res.json(err);
+    });
+}
+
 module.exports = {
-    getAllPostings
+    getAllPostings,
+    getPostingsSavedByUser,
+    getUsersFromPosting
 };
