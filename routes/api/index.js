0f3d4ac9184a93e908be2aa9b4b43f5f1e1d498e
Kevin Wu <peiyuwu9@gmail.com>
2019-12-02 10:58:11 -0500

    add api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,38 @@
 const router = require('express').Router();
 
-const { getAllPostings } = require("../../controllers/user-controller");
+const { 
+    getAllPostings,
+    getPostingsSavedByUser,
+    getUsersFromSavedPosting,
+    createSubscription,
+    createUser,
+    createPosting,
+    deleteSubscription,
+    getPostingByEmployer,
+    setPostingById
+ } = require("../../controllers/user-controller");
 
-router.route("/").get(getAllPostings);
+
+// 1. Login
+// "/login"
+
+// 2. Signup
+// "/signup"
+
+// Boss
+// 3. Employer posts showing all the jobs a boss posts
+// "/employerposts"
+router.route("/employerposts").get(getPostingByEmployer);
+
+// 4. Post detail showing job title, job description, who saved the posts
+// "/postdetails"
+router.route("/postdetails").get(setPostingById);
+
+// Seeker
+// 4. Job List
+// "/jobs"
+
+
+
 
 module.exports = router;
