fc6d23388d03bab380820a92e90b44bfdbb1feca
Kevin Wu <peiyuwu9@gmail.com>
2019-12-04 14:08:02 -0500

    Complet signup page, fix some extra codes

--- client/src/pages/JobDetail.js
+++ client/src/pages/JobDetail.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
-import { Link } from "react-router-dom";
+
+// import { Link } from "react-router-dom";
 
 class JobDetail extends Component {
-    state = {}
+    state = {
+        jobList: []
+    }
 
     render() {
         return (
             <Header>
             
             </Header>
+
+
         )
     }
 }
-
-
-
-
-
-
-
 
 export default JobDetail;
