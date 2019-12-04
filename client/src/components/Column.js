cc43ecda2c2cfc52f0470ddc56ee9ae8aa9ea755
Sandman105 <cpossandon@gmail.com>
2019-12-04 15:16:23 -0500

    card component

--- client/src/components/Column.js
+++ client/src/components/Column.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 const Column = () => {
 
+    return (
+        <div>
 
+            <Card body className="text-center">
+                <CardTitle>Special Title Treatment</CardTitle>
+                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                <Button>Go somewhere</Button>
+            </Card>
+
+
+
+
+
+
+        </div>
+
+
+
+
+    )
 
 
 
--- client/src/pages/EmployerPosts.js
+++ client/src/pages/EmployerPosts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Card from '../components/Card';
 import getPostingByEmployer from '../utils/API';
 
 // import { Link } from "react-router-dom";
@@ -11,19 +12,27 @@ class EmployerPosts extends Component {
     }
 
     componentDidMount = () => {
-        getPostingByEmployer
+        //getPostingByEmployer
     }
 
 
     render() {
         return (
-            <Header>
-                
-            </Header>
-            
-        )
-    }
-}
+            < >
+                <Header>
+
+                </Header>
+                <div>
+                    <Card>
+
+                    </Card>
+
+                </div>
+
+            </>
+        );
+    };
+};
 
 
 
