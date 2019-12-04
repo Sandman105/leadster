c88378e65f4f157dc0effdcee9ee49717ed20d62
Kevin Wu <peiyuwu9@gmail.com>
2019-12-04 11:20:53 -0500

    Update routes

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { login } from '../utils/API';
+import { readdir } from 'fs';
 //import { Link } from "react-router-dom";
 
 class Login extends Component {
@@ -29,8 +30,14 @@ class Login extends Component {
         }
         login(email, password)
         .then(
-            token => {
-                sessionStorage.setItem("jwt", token);
+            data => {
+                sessionStorage.setItem("jwt", data.token);
+                if (data.isEmployer === 0) {
+                    window.location.href("/community");
+                }
+                else {
+                    window.location.href("/employer-posts");
+                }
             }
         )
     };
