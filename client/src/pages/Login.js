4491c2e1e2755fa359167d1f614f1850e17b5697
Sandman105 <cpossandon@gmail.com>
2019-12-04 10:40:53 -0500

    header, form, and signup page/component, new package added to dependencies, edited login.js

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,21 +1,8 @@
 import React, { Component } from 'react';
-import Form from '../components/Form'
 import Jumbotron from '../components/Jumbotron'
 import { Link } from "react-router-dom";
 
 
-class Login extends Component {
-    state = {}
 
-    render() {
-        return (
-            
-                <Form>
-
-                </Form>
-            
-        );
-    };
-};
 
 export default Login;
