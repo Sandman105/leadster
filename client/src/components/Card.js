fdbcb619d35fc8e0b252d50e8a91bbc85bced1a1
Sandman105 <cpossandon@gmail.com>
2019-12-06 12:05:06 -0500

    update to CommunitySavedDetail.js, removed duplicate import

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Button, CardTitle, CardText } from 'reactstrap';
+import { Card, CardTitle, CardText } from 'reactstrap';
 
 
 const CardForm = ({ title, href}) => {
